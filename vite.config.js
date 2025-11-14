e943a3d8934ebcd8873175b4fda27e9c56d9e2f5
JIAN-LESTER <jianlestertabarno2014@gmail.com>
2025-11-15 01:03:20 +0800

    Vite Config Issues

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import flowbite from 'flowbite/plugin';
 
 export default defineConfig({
     plugins: [
@@ -9,6 +10,6 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
-        require('flowbite/plugin'),
+        flowbite,
     ],
 });
