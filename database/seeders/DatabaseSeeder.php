6b0804df8f072138b738d19fcec927c8d2bdccdf
cheesyGarlicBread15 <160604792+cheesyGarlicBread15@users.noreply.github.com>
2025-11-30 19:01:40 +0800

    added product seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             MembershipPlanSeeder::class,
             CategorySeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
